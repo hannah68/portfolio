723c8870f72ac2c96e1bec4493ba566d8897d26f
hannah68 <reihaneh.naderi1989@gmail.com>
2022-04-19 17:49:06 +0100

    working on navbar styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 import { Link } from "react-router-dom";
+import {useState} from 'react'
 import '../styles/Home.css';
+import {GrMenu} from 'react-icons/gr';
 
 const Header = () => {
+    const [isNavExpanded, setIsNavExpanded] = useState(false);
+
   return (
     <header>
         <nav className="navbar">
@@ -10,8 +14,11 @@ const Header = () => {
                     <img className="logo-img" src="../assets/images/small-logo.png" alt="reihaneh naderi"/>
                 </Link>
             </div>
+            <button className="hamburger" onClick={() => setIsNavExpanded(!isNavExpanded)}>
+                <GrMenu className="menu"/>
+            </button>
 
-            <ul className="navbar__lists">
+            <ul className={isNavExpanded ? 'navbar__lists expanded': 'navbar__lists'}>
                 <li className="navbar__lists--item">
                     <Link to='/'>Home</Link>
                 </li>
